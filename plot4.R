b30fc71f18ab760614962babcd35a9ce7c3b0c62
Alexis Sykut <parkeralexis@gmail.com>
2015-11-06 15:21:01 -0500

    Committing Project 1

--- plot4.R
+++ plot4.R
@@ -18,12 +18,27 @@ DayOfWeekLabel <- strftime(SDate, "%a")
 
 SDateTime <- strptime(paste(sData$Date, sData$Time), "%d/%m/%Y %H:%M") 
 
-##Plot 3
-png(file = "plot3.png", bg = "transparent", width = 480,  height = 480)
-with(sData, plot(SDateTime, sData$Sub_metering_1, xlab = "", ylab = "Energy sub metering", type = "n"))
+##Plot 4
+png(file = "plot4.png", bg = "transparent", width = 480,  height = 480)
+par(mfrow = c(2,2))
+with(sData, {
+     
+plot(SDateTime, sData$Global_active_power, xlab = "", ylab = "Global Active Power (kilowatts)", type = "n")
+lines(SDateTime, sData$Global_active_power)
+
+plot(SDateTime, sData$Voltage, xlab = "datetime", ylab = "Voltage", type = "n")
+lines(SDateTime, sData$Voltage)
+
+plot(SDateTime, sData$Sub_metering_1, xlab = "", ylab = "Energy sub metering", type = "n")
 lines(SDateTime, sData$Sub_metering_1, col = "black")
 lines(SDateTime, sData$Sub_metering_2, col = "red")
 lines(SDateTime, sData$Sub_metering_3, col = "blue")
 legend("topright", lwd = 1, col = c("black","red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+plot(SDateTime, sData$Global_reactive_power, xlab = "datetime", ylab = "Global_reactive_power", type = "n")
+lines(SDateTime, sData$Global_reactive_power)
+}
+            
+)
 dev.off()
 
